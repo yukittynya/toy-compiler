df821fda4bef7ecad2e7c36e696a9ec6f108e2ab
yukittynya <lichtestv@gmail.com>
2025-07-30 13:44:12 +0200

    started on parser

--- src/lib/parser/parser.h
+++ src/lib/parser/parser.h
@@ -3,6 +3,17 @@
 
 #include "../token/token.h"
 
+typedef enum {
+    // functions decl, call, print, let, binary ops, identifier, string literal, number literal
+} AstNodeType;
+
+typedef struct {
+    AstNodeType type;
+    union {
+        //statements
+    } data;
+} AstNode;
+
 typedef struct {
     TokenArray* tokens;
 } Parser;
